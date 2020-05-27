afe32500106be3b4342fa925e5305c4ee5dadd7b
Renjingkai <2964451385@qq.com>
2020-05-27 11:42:20 +0800

    update music

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# Westlife Using Flutter
-
+## Westlife Using Flutter
+Welcome Home Westlife fans,please click the like button,i will keep on working for this project.
 For Westlife Fans all around the world,Use Flutter to create Westlife apps run in iOS、Android、Web.
-It is a very initial versions project,I hope one day it could be Offical App.
+Flutter gives us oppourity to write once run in all Platform which is the ideal tool to finish this project.
+It is a very initial versions project,I don't want to call it a demo,i hope one day it could be Offical App,
+##If you know Flutter please help make contributes to this project,help us to improve user experience.
 If you have any good idea please email me 2964451385@qq.com
 
 
 ## Getting Started
 Get Flutter Enviroments, it works in iOS、Android、Web platform。
 
-### Preview
+## Preview(If you can‘t see,maybe you are in China,so please use VPN)
 iOS
  ![image](https://github.com/renjingkai/WestlifeUsingFlutter/blob/master/assets/githubImages/ios2.png?raw=true)
  Android
-  ![image](https://github.com/renjingkai/WestlifeUsingFlutter/blob/master/assets/githubImages/android1.png?raw=true)
+ ![image](https://github.com/renjingkai/WestlifeUsingFlutter/blob/master/assets/githubImages/android1.png?raw=true)
   Web
-   ![image](https://github.com/renjingkai/WestlifeUsingFlutter/blob/master/assets/githubImages/web1.png?raw=true)
+ ![image](https://github.com/renjingkai/WestlifeUsingFlutter/blob/master/assets/githubImages/web1.png?raw=true)
 
--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
          'Angel (BONUS Bad Girls)',
     ],
     'albumImage':'assets/Images/worldofourown.jpg',
-    
+    'intro':'World of Our Own is the third studio album recorded by Irish boy band Westlife. It was released through RCA Records on 1 November 2001. This is their third album release under RCA and Sony Music. It was also the band\'s third album to be released as a five-piece. It reached number one in the United Kingdom and includes the singles "Queen of My Heart" (which was a double A-side with "When You\'re Looking Like That"), "World of Our Own" (which was a double A-side with "Angel"), "Bop Bop Baby", and the final single from the group\'s last album, "Uptown Girl". "Evergreen" was later covered by Will Young as his winner\'s single for the 2002 Pop Idol competition. It is their first album to have a title track.\nThe album went 4x Platinum in the UK and is currently the fourteenth biggest selling boy band album ever in the UK.[2] In January 2005, the album was re-released in a 2-in-1 box set with the group\'s previous album, Coast to Coast.[3]'
     },
     {'album': 'Unbreakable','songs':[
       'Swear It Again',
@@ -149,7 +149,10 @@ class _MyHomePageState extends State<MyHomePage> {
       'Unbreakable',
       'Written In The Stars',
       'How Does It Feel'
-    ]},
+    ],'albumImage':'assets/Images/unbreakable.jpg',
+    'intro':'Unbreakable – The Greatest Hits Volume 1 is the first greatest hits album by Irish boy band Westlife. The album was released on 11 November 2002. It was also the band\'s fourth album to be released as a five-piece and with RCA Records and Sony BMG. The album consists of all of the group\'s past singles, along with six new songs. Hit single "Flying Without Wings" was re-recorded as a duet with South Korean singer BoA and Mexican singer Cristian Castro, and each respective duet was included on the Asian and Spanish editions of the album respectively. The first single released from the album was "Unbreakable", a UK number-one single. The second single, the double A-side "Tonight" / "Miss You Nights" peaked at number three in the UK and at number one in the Republic of Ireland.\nUnbreakable peaked at number one in the UK and sold close to 1.8 million copies in the UK alone. The album was the ninth best-selling album of 2002 in the UK. It is also the band\'s biggest selling album so far and their longest charting album. It managed to re-enter the year-end albums chart of 2007 at number 107. In October 2008, IFPI announced that the album was certified 2x Platinum, exceeding two million sales in Europe. On the year-end album charts of 2008, the album re-entered at number 79 with 191,000 copies sold within the year. The album spent 55 weeks on the UK Top 100 Albums chart.[1] This is the fifth best-selling album of 2002 in Ireland.\nIn 2008, the album was re-issued in an exclusive two disc edition, entitled "Unbreakable: The Greatest Hits - 2008 NZ Tour Edition". The package featured the album, with the same track listing, plus the "Live at Wembley" 2006 concert DVD. It peaked at No. 1 on the albums chart there, and achieved 2x Platinum status, with more than 30,000 copies sold. The album finished at No. 19 on the 2008 New Zealand Year-end charts.'
+    
+    },
     {'album': 'Turnaround','songs':[
       'Mandy',
       'Hey Whatever',
@@ -161,11 +164,40 @@ class _MyHomePageState extends State<MyHomePage> {
       'I Did it For You',
       'Thank You',
       'To Be With You'
-    ]},
+    ],'albumImage':'assets/Images/turnaround.jpg',
+    'intro':'Turnaround is the fifth studio album by Irish boy band Westlife, released on 24 November 2003 by BMG. It was also the band\'s fifth album to be released as a five-piece and with RCA Records and Sony BMG. The first single released was the upbeat track, "Hey Whatever". The next single was a cover of the Barry Manilow hit, "Mandy". The band\'s version earned them their 12th UK number one and an Irish record of the year award. "Obvious", an original song, was the third and final single released from the album.\nTurnaround was also the last album to be recorded with their full original lineup with member Brian McFadden, who leaves the band in March the following year. The album was the 23rd best selling album of 2003 in the UK. The album was re-released in a box set on 25 January 2005 with their debut album, Westlife. The album received favourable reviews from music critics, while it attained commercial success, topping the UK and Ireland charts, while reaching the top-ten in many countries.'
+    },
     {'album': 'Allow Us To Be Frank','songs':[
+    'Aint That A Kick In The Head',
+    'Fly Me',
+    'Smile',
+    'Let There Be Love',
+    'The Way You Look Tonight (feat Joanne Hindley) ',
+    'Come Fly With Me',
+    'Mack The Knife',
+    'I Left My Heart In San Francisco',
+    'Summer Wind',
+    'Clementine',
+    'When I Fall In Love',
+    'Moon River',
+    ],
+    'intro':'...Allow Us to Be Frank, a Rat Pack tribute, is the sixth studio album, sixth major album release under Sony BMG and first cover album by Irish boy band Westlife; it is also the first album since the departure of Bryan McFadden and as a four-piece. It was released on 8 November 2004, and peaked at number two in Ireland and number three in the United Kingdom. ...Allow Us To Be Frank was number twenty-four on the 2004 year-end album charts. The album features songs made popular by Frank Sinatra such as "The Way You Look Tonight", "Come Fly with Me", "Moon River", "Summer Wind" and "That\'s Life". It also includes the Nat "King" Cole song, "When I Fall in Love". It was recorded with a 60-piece orchestra at Phoenix Studios in Wembley, in the London Borough of Brent.','albumImage':'assets/Images/allowustobefrank.jpg'},
 
-    ]},
-    {'album': 'Face To Face'},
+
+    {'album': 'Face To Face','songs':[
+    'You Raise Me Up',
+    'When You Tell Me That You Love Me (FDiana Ross)',
+    'Amazing',
+    'Thats Where You Find Love',
+    'Shes Back ',
+    'Desperado',
+    'Colour My World',
+    'In This Life',
+    'Heart Without A Home',
+    'Hit You With The Real Thing',
+    ],
+    'intro':'Face to Face is the seventh studio album by Irish boy band Westlife and it was released in the UK on 31 October 2005. The album contains pop songs and some dance oriented tracks. It was also the band\'s second album to be released as a four-piece. It was also the band\'s seventh album release under RCA Records and Sony BMG.\nThe album was also released on DualDisc in the UK, which included the videos of the making of "You Raise Me Up", the Face to Face album photoshoot and exclusive interview footage.[5]\nThe album includes a cover of "She\'s Back" by Human Nature, "Desperado" by the Eagles, Collin Raye\'s number one hit "In This Life", and a Nick Carter solo song, "Heart Without a Home".The album had more than 10,000 legal copies sold in mainland China.[6]',
+    'albumImage':'assets/Images/facetoface.jpg'},
   ];
 
   
